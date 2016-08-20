ab9b95d7226485b0bdac75c347917d9b84b23305
Andrew Holway <a.holway@dcmn.com>
2016-08-20 14:46:45 +0200

    first commit

---
+++ mysite/networks/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from views import VpcCreate, VpcUpdate, VpcDelete, index, VpcFormView
+from . import views
+
+urlpatterns = [
+    url(r'^$', index, name='index'),
+    url(r'^vpc/add/?$', VpcCreate.as_view(), name='vpc-add'),
+    url(r'^vpc/addall/?$',VpcFormView.as_view(), name='vpc-add-all'),
+    url(r'^vpc/(?P<pk>[0-9]+)/?$', VpcUpdate.as_view(), name='vpc-update'),
+    url(r'^vpc/(?P<pk>[0-9]+)/delete/?$', VpcDelete.as_view(), name='vpc-delete'),
+]
---
+++ mysite/networks/views.py
@@ -0,0 +1,55 @@
+import boto
+from django.core.urlresolvers import reverse_lazy
+from django.shortcuts import redirect
+from django.template.response import TemplateResponse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, View
+from .forms import InstanceForm, SubnetForm, VpcForm
+
+from models import Vpc
+
+
+def index(request):
+    conn = boto.connect_vpc()
+    vpc_list = conn.get_all_vpcs()
+    return TemplateResponse(request, 'networks/index.html', {'vpc_list': vpc_list})
+
+
+class VpcCreate(CreateView):
+    model = Vpc
+    fields = ['name', 'cidr']
+
+
+class VpcUpdate(UpdateView):
+    model = Vpc
+    fields = ['name', 'cidr']
+
+
+class VpcDelete(DeleteView):
+    model = Vpc
+    success_url = reverse_lazy('vpc-list')
+
+
+class VpcFormView(View):
+    template_name = "networks/vpc_form.html"
+
+    def get(self, request, *args, **kwargs):
+        context = dict()
+        context['formA'] = VpcForm()
+        context['formB'] = SubnetForm()
+        context['formC'] = InstanceForm()
+        return TemplateResponse(request, template=self.template_name, context=context)
+
+    def post(self, request, *args, **kwargs):
+        vpc = VpcForm(request.POST)
+        subnet = SubnetForm(request.POST)
+        instance = InstanceForm(request.POST)
+        if vpc.is_valid() and subnet.is_valid() and instance.is_valid():
+            vpc = vpc.save()
+            subnet.instance.vpc = vpc
+            subnet = subnet.save()
+            instance.instance.subnet = subnet
+            instance.save()
+            return redirect("/")
+
+        return TemplateResponse(request, template=self.template_name,
+                                context={'formA': vpc, 'formB': subnet, 'formC': instance})
